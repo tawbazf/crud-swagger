82ee953946be5ba68461c4a76f1aa3288ecb62eb
tawbazf <tawbazehaf@gmail.com>
2025-03-19 10:12:48 +0100

    index in product controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,48 +1,44 @@
 <?php
+ namespace App\Http\Controllers;
 
-namespace App\Http\Controllers;
-
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
+public function index()
+{
+return Product::all();
+}
+/**
+* Store a newly created resource in storage.
+*/
+public function store(Request $request)
+{
+//
+}
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+/**
+* Display the specified resource.
+*/
+public function show(string $id)
+{
+//
+}
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
+/**
+* Update the specified resource in storage.
+*/
+public function update(Request $request, string $id)
+{
+//
+}
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
+/**
+* Remove the specified resource from storage.
+*/
+public function destroy(string $id)
+{
+//
+}
 }
